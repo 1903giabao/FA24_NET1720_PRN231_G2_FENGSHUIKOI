6df4b1fc414a6be63522e4bfb3b336733a70aaab
tuanhuu3264 <tuanhuu3264@gmail.com>
2024-09-15 17:22:30 +0700

    fix-prj-struct

---
+++ FA24_NET1720_PRN231_G2_FENGSHUIKOI/Source/FA24_NET1720_PRN231_G2_FENGSHUIKOI/FENGSHUIKOI.Data/Enums/ElementObjects.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FENGSHUIKOI.Data.Enums
+{
+    public class ElementObjects
+    {
+        public static readonly Dictionary<int, string> ThienCan = new Dictionary<int, string>
+        {
+        { 0, "Giáp" }, {1, "Ất"} , {2, "Bính" }, {3, "Đinh"} , {4, "Mậu"}, {5, "Kỉ"}, {6,"Canh"}, {7,"Tân"}, {8, "Nhâm"}, {9,"Quý"}
+        } ;
+        public static readonly Dictionary<int, string> DiaChi = new Dictionary<int, string>
+        {
+        { 0, "Tý" }, {1, "Sửu"} , {2, "Dần" }, {3, "Mão"} , {4, "Thìn"}, {5, "Tị"}, {6,"Ngọ"}, {7,"Mùi"}, {8, "Thân"}, {9,"Dậu"} , {10,"Tuất"}, {11, "Hợi"}
+        };
+        public static readonly Dictionary<string, int> ConventionThienCan = new Dictionary<string, int>
+        {
+            {"Giáp",1 },{"Ất",1 },
+            {"Bính",2 }, {"Đinh",2},
+            {"Mậu",3 }, {"Kỷ",3},
+            {"Canh",4 }, {"Tân",4},
+            {"Nhâm",5 }, {"Quý",5}
+        };
+        public static readonly Dictionary<string, int> ConventionDiaChi = new Dictionary<string, int>
+        {
+            {"Tý",0 },{"Sửu",0 },{"Ngọ",0 }, {"Mùi",0 },
+            {"Dần",1 }, {"Mão",1},{"Thân",1},{"Dậu",1},
+            {"Thìn",2 }, {"Tỵ",2}, {"Tuất",2}, {"Hợi",2}
+        };
+        public static readonly Dictionary<int, string> MaleValueInElement = new Dictionary<int, string>
+        {
+            {1,"Khảm" }, {2, "Ly"}, {3, "Cấn"}, {4,"Đoài"}, {5, "Càn"}, {6,"Khôn"}, {7, "Tốn"}, {8, "Chấn"}, {0, "Khôn"}
+        };
+        public static readonly Dictionary<int, string> FemaleValueInElement = new Dictionary<int, string>
+        {
+            {6,"Khảm" }, {5, "Ly"}, {1, "Cấn"}, {3,"Đoài"}, {2, "Càn"}, {7,"Khôn"}, {0, "Tốn"}, {8, "Chấn"}, {4, "Cấn"}
+        };
+
+        public static readonly Dictionary<int, string> ElementValue = new Dictionary<int, string>
+        {
+            {1,"Kim" }, {2, "Thủy"}, {3, "Hỏa"}, {4,"Thổ"}, {5, "Mộc"}
+        };
+    }
+}
---
+++ FA24_NET1720_PRN231_G2_FENGSHUIKOI/Source/FA24_NET1720_PRN231_G2_FENGSHUIKOI/FENGSHUIKOI.Service/Services/ElementService.cs
@@ -0,0 +1,221 @@
+﻿using FENGSHUIKOI.Common;
+using FENGSHUIKOI.Data.Enums;
+using FENGSHUIKOI.Data.Models;
+using FENGSHUIKOI.Data.UnitOfWork;
+using FENGSHUIKOI.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FENGSHUIKOI.Service.Services
+{
+    public interface IElementService
+    {
+        Task<IBusinessResult> GetAll();
+        Task<IBusinessResult> GetById(int id);
+        Task<IBusinessResult> Update(Element element);
+        Task<IBusinessResult> DeleteById(int id);
+        Task<IBusinessResult> Save(Element element);
+        Task<IBusinessResult> CaculateElement(string gender, int year);
+    }
+    public class ElementService : IElementService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+
+        public ElementService()
+        {
+            _unitOfWork ??= new UnitOfWork(); 
+
+        }
+
+
+        public async Task<IBusinessResult> DeleteById(int id)
+        {
+            try
+            {
+                
+                var payment = await _unitOfWork.ElementRepository.GetByIdAsync(id);
+                if (payment != null)
+                {
+        
+                    var result = await _unitOfWork.ElementRepository.RemoveAsync(payment);
+                    if (result)
+                    {
+                        return new BusinessResult(Const.SUCCESS_DELETE, Const.SUCCESS_DELETE_MSG);
+                    }
+                    else
+                    {
+                        return new BusinessResult(Const.FAIL_DELETE, Const.FAIL_DELETE_MSG);
+                    }
+                }
+                else
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> GetAll()
+        {
+            try
+            {
+                #region Business rule
+                #endregion
+
+                var payments = await _unitOfWork.ElementRepository.GetAllAsync();
+
+                if (payments == null)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.SUCCESS_READ, Const.SUCCESS_READ_MSG, payments);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(Const.ERROR_EXEPTION, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> GetById(int id)
+        {
+            try
+            {
+                #region Business rule
+                #endregion
+
+
+                var payment = await _unitOfWork.ElementRepository.GetByIdAsync(id);
+
+                if (payment == null)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.SUCCESS_READ, Const.SUCCESS_READ_MSG, payment);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+       
+        public async Task<IBusinessResult> Save(Element payment)
+        {
+            try
+            {
+                int result = await _unitOfWork.ElementRepository.CreateAsync(payment); 
+                if (result > 0)
+                {
+                    return new BusinessResult(Const.SUCCESS_CREATE, Const.SUCCESS_CREATE_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> Update(Element payment)
+        {
+            try
+            {
+
+                int result = await _unitOfWork.ElementRepository.UpdateAsync(payment);
+                if (result > 0)
+                {
+                    return new BusinessResult(Const.FAIL_UDATE, Const.SUCCESS_UDATE_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.FAIL_UDATE, Const.FAIL_UDATE_MSG);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+        public async Task<string> CaculateThienCan(int year)
+        {
+            var thienCanValue = year % 9;
+            var thienCan = ElementObjects.ThienCan.GetValueOrDefault(thienCanValue);
+            return thienCan;
+        }
+
+        public async Task<string> CaculateDiaChi(int year)
+        {
+            var diaChiValue = year % 12;
+            var diaChi = ElementObjects.DiaChi.GetValueOrDefault(diaChiValue);
+            return diaChi;
+        }
+
+        public async Task<string> CaculateCungMenh(int year, string gender)
+        {
+            var normalizedGender = gender.ToLower();
+
+            var previousElement = normalizedGender == "male"
+                ? ElementObjects.MaleValueInElement.GetValueOrDefault(year % 9)
+                : ElementObjects.FemaleValueInElement.GetValueOrDefault(year % 9);
+            return previousElement;
+        }
+
+
+        public async Task<IBusinessResult> CaculateElement(string gender, int year)
+        {
+            try
+            {
+
+                if (gender.ToLower() != "male" && gender.ToLower() != "female") return new BusinessResult(400, "The gender is not suitable.");
+                if (year < 0) return new BusinessResult(400, "The year is not suitable.");
+
+                var normalizedGender = gender.ToLower();
+
+                var previousElement = normalizedGender == "male"
+                    ? ElementObjects.MaleValueInElement.GetValueOrDefault(year % 9)
+                    : ElementObjects.FemaleValueInElement.GetValueOrDefault(year % 9);
+
+                var thienCanValue = year % 9;
+                var diaChiValue = year % 12;
+
+                var thienCan = ElementObjects.ThienCan.GetValueOrDefault(thienCanValue);
+                var diaChi = ElementObjects.DiaChi.GetValueOrDefault(diaChiValue);
+
+                var elementValue = diaChiValue + thienCanValue;
+                if (elementValue > 5) elementValue -= 5; 
+
+                var element = ElementObjects.ElementValue.GetValueOrDefault(elementValue);
+
+                return new BusinessResult(200,"Caculating element is success" , new
+                {
+                    ThienCan = thienCan,
+                    DiaChi = diaChi,
+                    Menh = previousElement + " " + element,
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+    }
+}
