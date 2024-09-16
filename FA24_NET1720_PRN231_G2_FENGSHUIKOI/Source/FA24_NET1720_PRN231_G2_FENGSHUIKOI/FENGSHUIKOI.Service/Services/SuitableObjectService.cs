e978bc920253edc7f1400166e49ba2ca5bbfccb1
1903giabao <baogavip00@gmail.com>
2024-09-16 08:42:16 +0700

    Bao/SuitableObjectService

---
+++ FA24_NET1720_PRN231_G2_FENGSHUIKOI/Source/FA24_NET1720_PRN231_G2_FENGSHUIKOI/FENGSHUIKOI.Service/Services/SuitableObjectService.cs
@@ -0,0 +1,155 @@
+﻿using FENGSHUIKOI.Common;
+using FENGSHUIKOI.Data.Models;
+using FENGSHUIKOI.Data.UnitOfWork;
+using FENGSHUIKOI.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FENGSHUIKOI.Service.Services
+{
+    public interface ISuitableObjectService
+    {
+        Task<IBusinessResult> GetAll();
+        Task<IBusinessResult> GetById(int id);
+        Task<IBusinessResult> Update(SuitableObject obn);
+        Task<IBusinessResult> DeleteById(int id);
+        Task<IBusinessResult> Save(SuitableObject obj);
+    }
+    public class SuitableObjectService : ISuitableObjectService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+
+        public SuitableObjectService()
+        {
+            _unitOfWork ??= new UnitOfWork();
+
+        }
+
+
+        public async Task<IBusinessResult> DeleteById(int id)
+        {
+            try
+            {
+
+                var obj = await _unitOfWork.SuitableObjectRepository.GetByIdAsync(id);
+                if (obj != null)
+                {
+
+                    var result = await _unitOfWork.SuitableObjectRepository.RemoveAsync(obj);
+                    if (result)
+                    {
+                        return new BusinessResult(Const.SUCCESS_DELETE, Const.SUCCESS_DELETE_MSG);
+                    }
+                    else
+                    {
+                        return new BusinessResult(Const.FAIL_DELETE, Const.FAIL_DELETE_MSG);
+                    }
+                }
+                else
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> GetAll()
+        {
+            try
+            {
+                #region Business rule
+                #endregion
+
+                var objs = await _unitOfWork.SuitableObjectRepository.GetAllAsync();
+
+                if (objs == null)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.SUCCESS_READ, Const.SUCCESS_READ_MSG, objs);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(Const.ERROR_EXEPTION, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> GetById(int id)
+        {
+            try
+            {
+                #region Business rule
+                #endregion
+
+
+                var obj = await _unitOfWork.SuitableObjectRepository.GetByIdAsync(id);
+
+                if (obj == null)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA, Const.WARNING_NO_DATA_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.SUCCESS_READ, Const.SUCCESS_READ_MSG, obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+
+        public async Task<IBusinessResult> Save(SuitableObject obj)
+        {
+            try
+            {
+                int result = await _unitOfWork.SuitableObjectRepository.CreateAsync(obj);
+                if (result > 0)
+                {
+                    return new BusinessResult(Const.SUCCESS_CREATE, Const.SUCCESS_CREATE_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+
+        public async Task<IBusinessResult> Update(SuitableObject obj)
+        {
+            try
+            {
+
+                int result = await _unitOfWork.SuitableObjectRepository.UpdateAsync(obj);
+                if (result > 0)
+                {
+                    return new BusinessResult(Const.FAIL_UDATE, Const.SUCCESS_UDATE_MSG);
+                }
+                else
+                {
+                    return new BusinessResult(Const.FAIL_UDATE, Const.FAIL_UDATE_MSG);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(-4, ex.Message);
+            }
+        }
+    }
+}
