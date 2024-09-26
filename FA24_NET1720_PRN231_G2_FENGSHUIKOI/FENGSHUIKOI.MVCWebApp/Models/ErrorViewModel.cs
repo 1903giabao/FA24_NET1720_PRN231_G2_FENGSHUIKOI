6d9f915cf2db663dfea2833ab9bc38f00974af4a
1903giabao <baogavip00@gmail.com>
2024-09-26 08:02:41 +0700

    change path

---
+++ FA24_NET1720_PRN231_G2_FENGSHUIKOI/FENGSHUIKOI.MVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FENGSHUIKOI.MVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
