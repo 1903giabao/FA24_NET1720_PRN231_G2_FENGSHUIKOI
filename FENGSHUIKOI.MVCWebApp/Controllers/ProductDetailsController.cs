9b7370bb5e35263c78c6b2ca546bff00df14e552
1903giabao <baogavip00@gmail.com>
2024-09-26 21:15:28 +0700

    mvcSuitableObject

---
+++ FENGSHUIKOI.MVCWebApp/Controllers/ProductDetailsController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FENGSHUIKOI.Data.Models;
+using FENGSHUIKOI.Common;
+using Newtonsoft.Json;
+using FENGSHUIKOI.Service.Base;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FENGSHUIKOI.MVCWebApp.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        private readonly NET1720_231_2_FENGSHUIKOIContext _context;
+
+        public ProductDetailsController(NET1720_231_2_FENGSHUIKOIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductDetails
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = new  HttpClient())
+            {
+                try
+                {
+                    using (var response = await httpClient.GetAsync(Const.APIEndPoint + "ProductDetail")) 
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Data != null)
+                            {
+                                var data = JsonConvert.DeserializeObject<List<ProductDetail>>(result.Data.ToString());
+                                return View(data);
+                            }
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    // Log the exception (consider using a logging framework)
+                    Console.WriteLine($"Request error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    // Catch any other exceptions
+                    Console.WriteLine($"Unexpected error: {ex.Message}");
+                }
+            }
+            return View(new List<ProductDetail>());
+        }
+
+        // GET: ProductDetails/Details/5
+/*        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails
+                .Include(p => p.TypeNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDetail);
+        }
+
+        // GET: ProductDetails/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Name");
+            return View();
+        }
+
+        // POST: ProductDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description,ComboId,TypeId,Color,Name,Quantity,Type,Status,CreateDate,Size,Origin")] ProductDetail productDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Name", productDetail.TypeId);
+            return View(productDetail);
+        }
+
+        // GET: ProductDetails/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails.FindAsync(id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Name", productDetail.TypeId);
+            return View(productDetail);
+        }
+
+        // POST: ProductDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,ComboId,TypeId,Color,Name,Quantity,Type,Status,CreateDate,Size,Origin")] ProductDetail productDetail)
+        {
+            if (id != productDetail.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductDetailExists(productDetail.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Name", productDetail.TypeId);
+            return View(productDetail);
+        }
+
+        // GET: ProductDetails/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productDetail = await _context.ProductDetails
+                .Include(p => p.TypeNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDetail);
+        }
+
+        // POST: ProductDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productDetail = await _context.ProductDetails.FindAsync(id);
+            if (productDetail != null)
+            {
+                _context.ProductDetails.Remove(productDetail);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductDetailExists(int id)
+        {
+            return _context.ProductDetails.Any(e => e.Id == id);
+        }*/
+    }
+}
